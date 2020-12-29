71893e87474f05e5e587cd526fe9b046e56728bc
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-29 14:43:26 +0300

    initial commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Waveline Media on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
